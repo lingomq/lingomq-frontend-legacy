a446a1694ba5666fac910cb75b7f6629412bddc9
prnic <domarlaid2@mail.ru>
2023-11-01 08:30:15 +0500

    add footer component

--- src/components/structure/footers/default-footer/footer.jsx
+++ src/components/structure/footers/default-footer/footer.jsx
@@ -4,41 +4,50 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-navigation">
-                <p className="footer-navigation-main-p">Карта сайта</p>
+                <p className="footer-navigation-main-p">КАРТА САЙТА</p>
                 <ul className="footer-navigation-ul">
                     <li className="footer-navigation-li">
                         <a className="footer-navigation-li-a" href="">
-                            Главная
+                            НАЧАТЬ
                         </a>
                     </li>
                     <li className="footer-navigation-li">
                         <a className="footer-navigation-li-a" href="">
-                            Начать
+                            О НАС
                         </a>
                     </li>
                     <li className="footer-navigation-li">
                         <a className="footer-navigation-li-a" href="">
-                            О нас
+                            ПОДДЕРЖКА
                         </a>
                     </li>
                     <li className="footer-navigation-li">
                         <a className="footer-navigation-li-a" href="">
-                            Поддержка
+                            РЕЙТИНГ
                         </a>
                     </li>
                     <li className="footer-navigation-li">
                         <a className="footer-navigation-li-a" href="">
-                            Рейтинг
+                            ПОЛИТИКА КОНФИДЕНЦИАЛЬНОСТИ
+                        </a>
+                    </li>
+                    <li className="footer-navigation-li">
+                        <a className="footer-navigation-li-a" href="">
+                            ПОЛЬЗОВАТЕЛЬСКОЕ СОГЛАШЕНИЕ
                         </a>
                     </li>
                 </ul>
             </div>
             <div className="footer-last-section">
-                <div className="footer-logo">
-                    <p className="footer-logo-main">Lingo</p>
-                    <p className="logo-sub">mq</p>
+                <div className="footer-logo-section">
+                    <div className="footer-logo">
+                        <p className="footer-logo-main">lingo</p>
+                        <p className="footer-logo-sub">mq</p>
+                    </div>
+                    <p className="footer-copyright">
+                        © LingoMq. Все права защищены
+                    </p>
                 </div>
-                <p className="footer-copyright">© LingoMq. Все права защищены</p>
             </div>
         </div>
     );
