713c67be357b1f2d65673ad375b7eb7b4a52cc52
prnic <domarlaid2@mail.ru>
2023-11-01 09:40:06 +0500

    update header and add home -> main section

--- src/components/common/local-images.jsx
+++ src/components/common/local-images.jsx
@@ -2,8 +2,9 @@ import MainPagePicture from '../../../public/img/main-page-picture.svg';
 import DiaryBigPicture from '../../../public/img/diary-big-picture.svg';
 import RepeatBigPicture from '../../../public/img/repeat-big-picture.svg';
 import HamburgerMenuPicture from '../../../public/img/hamburger-menu.svg';
+import Spread from '../../../public/img/spread.svg';
 
 const Images = { MainPagePicture, DiaryBigPicture, RepeatBigPicture,
-    HamburgerMenuPicture }
+    HamburgerMenuPicture, Spread }
 
 export default Images;
--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -5,53 +5,18 @@ import Footer from "../../structure/footers/default-footer/footer.jsx";
 const Home = () => {
     return (
         <div className="content">
-            <div className="content-main">
-                <img src={Images.MainPagePicture} />
-                <div className="content-main-right">
-                    <h1 className="main-h2-alternative">Выучи новый язык</h1>
-                    <h2 className="main-h2-cursive">своими силами</h2>
-                    <p>
-                        Наше приложение - интерактивный словарь слов, с
-                        возможностью попрактиковаться, посмотреть статистику,
-                        похвастаться результатами
-                    </p>
-                    <button className="default-button">Начать</button>
-                </div>
-            </div>
-            <div className="content-short-about-us">
-                <h1>Ну и что нужно делать?</h1>
-                <div className="content-short-about-us-main">
-                    <div className="content-short-about-us-main-left">
-                        <p className="p-short-about-us-head">
-                            Для начала, достаточно скачать приложение, и
-                            добавить пару-тройку слов, а дальше все просто:
-                        </p>
-                        <ul className="ul-short-about-us">
-                            <li className="li-short-about-us">
-                                Каждый день добавляй новые слова
-                            </li>
-                            <li className="li-short-about-us">
-                                Повторяй эти слова в течение дня
-                            </li>
-                            <li className="li-short-about-us">
-                                Выработай привычку учить что-то новое и
-                                продолжай развиваться
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="content-short-about-us-main-right">
-                        <img src={Images.DiaryBigPicture} />
+            <div className="content-main-section">
+                <h1>УЧИ СЛОВА ВМЕСТЕ С</h1>
+                <div className="logo-big-section">
+                    <img src={Images.Spread} />
+                    <div className="logo">
+                        <p className="logo-main">Lingo</p>
+                        <p className="logo-sub">mq</p>
                     </div>
                 </div>
-            </div>
-            <div className="content-repeating-block">
-                <img src={Images.RepeatBigPicture} />
-                <div className="content-repeating-block-description">
-                    <p className="p-repeating-block-head">
-                        Повторяя слова каждый день, мозг начнет запоминать это
-                        все на автомате, чем-то похоже на детство, не так ли?
-                    </p>
-                </div>
+                <button className="default-button">
+                    ПОДРОБНЕЕ
+                </button>
             </div>
             <Footer />
         </div>
